0358099af5a34419848dd566d5b7a6a071d24a32
Matt Goodman <matt@pop-os.dreyln.dev>
2024-07-23 20:26:44 -0600

    Switched to catppuccin theme, removed sessions integration, added smart-splits

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -1,20 +1,3 @@
 return {
-  {
-    'natecraddock/workspaces.nvim',
-    opts = {
-      hooks = {
-        open_pre = {
-          -- If recording, save current session state and stop recording
-          'SessionsStop',
-
-          -- delete all buffers (does not save changes)
-          'silent %bdelete!',
-        },
-        open = function()
-          require('sessions').load('.session', { silent = true })
-        end,
-      },
-    },
-  },
-  { 'natecraddock/sessions.nvim', opts = {} },
+  { 'natecraddock/workspaces.nvim' },
 }
