8db27a6ab172c563004a0993c315a3aace8f0a3d
Matt Goodman <matt@pop-os.dreyln.dev>
2024-07-25 22:57:54 -0600

    corn is causing problems on buffer delete

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -207,16 +207,16 @@ return {
   {
     'ray-x/lsp_signature.nvim',
   },
-  {
-    'dreyln/corn.nvim',
-    event = 'VeryLazy',
-    opts = {
-      on_toggle = function(_)
-        vim.diagnostic.config { virtual_text = not vim.diagnostic.config().virtual_text }
-      end,
-      item_preprocess_func = function(item)
-        return item
-      end,
-    },
-  },
+  -- {
+  --   'dreyln/corn.nvim',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     on_toggle = function(_)
+  --       vim.diagnostic.config { virtual_text = not vim.diagnostic.config().virtual_text }
+  --     end,
+  --     item_preprocess_func = function(item)
+  --       return item
+  --     end,
+  --   },
+  -- },
 }
