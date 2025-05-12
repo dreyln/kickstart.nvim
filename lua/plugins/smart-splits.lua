eb512086e68332c7927c01e05f1c82628d653bbc
Matt Goodman <matt.goodman@securitymetrics.com>
2025-05-12 16:40:04 -0600

    Changed resize-mode in smart-splits due to breaking change and pinned Mason

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -29,18 +29,44 @@ return {
         end,
       },
       -- resize mode
-      {
-        '<leader>m',
-        function()
-          require('smart-splits').start_resize_mode()
+      -- {
+      --   '<leader>m',
+      --   function()
+      --     require('smart-splits').start_resize_mode()
+      --   end,
+      -- },
+    },
+    dependencies = {
+      'pogyomo/submode.nvim',
+    },
+    config = function()
+      -- Resize
+      local submode = require 'submode'
+      submode.create('WinResize', {
+        mode = 'n',
+        enter = '<leader>m',
+        leave = '<CR>',
+        hook = {
+          on_enter = function()
+            vim.notify 'Use { h, j, k, l } to resize the window'
+          end,
+          on_leave = function()
+            vim.notify ''
+          end,
+        },
+        default = function(register)
+          register('h', require('smart-splits').resize_left, { desc = 'Resize left' })
+          register('j', require('smart-splits').resize_down, { desc = 'Resize down' })
+          register('k', require('smart-splits').resize_up, { desc = 'Resize up' })
+          register('l', require('smart-splits').resize_right, { desc = 'Resize right' })
         end,
-      },
-    },
-    opts = {
-      ignored_filetypes = { 'NvimTree', 'neo-tree' },
-      resize_mode = {
-        quit_key = '<CR>',
-      },
-    },
+      })
+    end,
+    -- opts = {
+    --   ignored_filetypes = { 'NvimTree', 'neo-tree' },
+    --   resize_mode = {
+    --     quit_key = '<CR>',
+    --   },
+    -- },
   },
 }
