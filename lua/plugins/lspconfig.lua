6dcc216c7e8df01bc2656b687742caebb9212a70
Matt Goodman <matt.goodman@securitymetrics.com>
2025-10-17 11:38:49 -0600

    Use Ristretto for theme and enable lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,6 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    enabled = false,
     event = { 'BufReadPre', 'BufNewFile' },
   },
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,49 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  opts = {
-    integrations = {
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { 'undercurl' },
-          hints = { 'undercurl' },
-          warnings = { 'undercurl' },
-        },
-      },
-    },
-  },
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+  {
+    'loctvl842/monokai-pro.nvim',
+    config = function()
+      require('monokai-pro').setup {
+        filter = 'ristretto', -- classic | machine | octagon | ristretto | spectrum
+        override = function(c)
+          local hp = require("monokai-pro.color_helper")
+          local common_fg = hp.lighten(c.sideBar.foreground, 30)
+          return {
+            SnacksPicker = { bg = c.editor.background, fg = common_fg },
+            SnacksPickerBorder = { bg = c.editor.background, fg = c.tab.unfocusedActiveBorder },
+            SnacksPickerTree = { fg = c.editorLineNumber.foreground },
+            NonText = { fg = c.base.dimmed3 }, -- not sure if this should be broken into all hl groups importing NonText
+            -- SnacksPickerDirectory = { fg = c.editorLineNumber.foreground },
+          }
+        end,
+      }
+      vim.cmd 'colorscheme monokai-pro'
+    end
+  }
 }
-
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   opts = {
+--     integrations = {
+--       native_lsp = {
+--         enabled = true,
+--         underlines = {
+--           errors = { 'undercurl' },
+--           hints = { 'undercurl' },
+--           warnings = { 'undercurl' },
+--         },
+--       },
+--     },
+--   },
+--   init = function()
+--     -- Load the colorscheme here.
+--     -- Like many other themes, this one has different styles, and you could load
+--     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--     vim.cmd.colorscheme 'catppuccin'
+--   end,
+-- }
+--
 -- return {
 --   { -- You can easily change to a different colorscheme.
 --     -- Change the name of the colorscheme plugin below, and then
